d4c561206787a730443aeea03a3c8aa8733203c6
Mustakim <m.m.kiron@gmail.com>
2023-07-14 03:33:39 +0600

    Fix linter errors

--- navigation.js
+++ navigation.js
@@ -7,30 +7,30 @@ const addNewSection = document.querySelector('.add-new-section');
 const contactSection = document.querySelector('.contact-section');
 
 list.addEventListener('click', (event) => {
-    event.preventDefault();
-    if (bookListSection.classList.contains('hide')) {
-        bookListSection.classList.remove('hide');
-    }
-    addNewSection.classList.add('hide');
-    contactSection.classList.add('hide');
+  event.preventDefault();
+  if (bookListSection.classList.contains('hide')) {
+    bookListSection.classList.remove('hide');
+  }
+  addNewSection.classList.add('hide');
+  contactSection.classList.add('hide');
 });
 
 addNew.addEventListener('click', (event) => {
-    event.preventDefault();
-    if (addNewSection.classList.contains('hide')) {
-        addNewSection.classList.remove('hide');
-    }
-    bookListSection.classList.add('hide');
-    contactSection.classList.add('hide');
+  event.preventDefault();
+  if (addNewSection.classList.contains('hide')) {
+    addNewSection.classList.remove('hide');
+  }
+  bookListSection.classList.add('hide');
+  contactSection.classList.add('hide');
 });
 
 contact.addEventListener('click', (event) => {
-    event.preventDefault();
-    if (contactSection.classList.contains('hide')) {
-        contactSection.classList.remove('hide');
-    }
-    bookListSection.classList.add('hide');
-    addNewSection.classList.add('hide');
+  event.preventDefault();
+  if (contactSection.classList.contains('hide')) {
+    contactSection.classList.remove('hide');
+  }
+  bookListSection.classList.add('hide');
+  addNewSection.classList.add('hide');
 });
 
 const dateTime = document.querySelector('.date-time');
